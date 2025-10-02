e6e7353033bc74a974e956bad845cf4bfadfae00
Sergio <das0030045@est.univalle.edu>
2025-10-02 15:43:53 -0400

    P1 y p2 score y highscore

--- Assets/02_Scripts/Marcador.cs
+++ Assets/02_Scripts/Marcador.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Marcador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        PongScore.ScoreManager.Instance.ResetScores();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Llama esto cuando decidas fin de partida (o al volver al menú)
+    public void GuardarRecord()
     {
-        
+        PongScore.ScoreManager.Instance.SaveIfHighScore();
     }
 }
